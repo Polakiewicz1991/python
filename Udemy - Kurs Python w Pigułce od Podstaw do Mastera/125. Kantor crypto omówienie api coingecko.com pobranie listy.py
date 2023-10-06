e7934f44048c919be1049d9a2a79171001dbf5b2
Polakiewicz1991 <p.polakiewicz@konstruktion.pl>
2023-10-06 15:48:56 +0200

    gotowy kalkulator krypto

--- Udemy - Kurs Python w Pigułce od Podstaw do Mastera/125. Kantor crypto omówienie api coingecko.com pobranie listy.py
+++ Udemy - Kurs Python w Pigułce od Podstaw do Mastera/125. Kantor crypto omówienie api coingecko.com pobranie listy.py
@@ -1,6 +1,9 @@
+#https://www.coingecko.com/en/api/documentation
+
 import requests
 
 coinsList = None
+currency = 'pln'
 
 def getCoinsList():
     global coinsList
@@ -28,7 +31,57 @@ def findCoinBySymbol(symbol):
     else:
         print("nie udało się")
         return None
+
+
+def getCoinLastMaketData(CoinID = "bitcoin"):
+    if CoinID is not None:
+        #{'bitcoin': {'pln': 119473, 'pln_market_cap': 2338452805220.1543, 'pln_24h_vol': 62375284222.95747, 'pln_24h_change': -2.541370559386113, 'last_updated_at': 1696598947}}
+        response = requests.get(f"https://api.coingecko.com/api/v3/simple/price?ids={CoinID}&vs_currencies={currency}&include_market_cap=true&include_24hr_vol=true&include_24hr_change=true&include_last_updated_at=true&precision=true")
+        if response.ok == True:
+            print("response OK")
+            data = response.json()
+            return data
+        else:
+            print("response nOK")
+    else:
+        print("nie udało się")
+        return None
+
+def getCoinPriceInCurrency(CoinID,currency):
+    currency = currency.lower().strip()
+    marketData = getCoinLastMaketData(CoinID)
+    return marketData[CoinID][currency]
+
+
+
+
 getCoinsList()
 
-btcData = findCoinBySymbol("btc")
-print(btcData)
+coinData = findCoinBySymbol("btc")
+print(coinData)
+
+marketData = getCoinLastMaketData(coinData['id'])
+print(marketData)
+
+coinPrice = getCoinPriceInCurrency(coinData['id'],currency)
+print("Na złotówki: ", coinPrice)
+
+
+print("Kalkulator krypto")
+
+while True:
+    cryptoSymbolToBuy = input("Wybierz symbol krypto do kupienia lub wpisz exit, aby zakonczyć:")
+    if cryptoSymbolToBuy == "exit": break
+
+    coinData = findCoinBySymbol(cryptoSymbolToBuy)
+    if coinPrice == None:
+        print("Nie ma takiej waluty")
+        continue
+
+    coinPrice = getCoinPriceInCurrency(coinData['id'], currency)
+    print(f"{coinData['id']} na złotówki: ", coinPrice)
+
+    moneyToBuyCrypto = float(input("Ile chcesz przeznaczyć na zakup: "))
+    boughtCrypto = moneyToBuyCrypto / coinPrice
+
+    print(f"zakupiono {boughtCrypto} {coinData['id']}")

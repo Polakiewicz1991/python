e7d3148729cae36f0efd5be1f03a7133e4385a02
Polakiewicz1991 <polakiewicz1991@gmail.com>
2023-12-15 11:02:33 +0100

    Zadanie proste kinematyki - tworzenie animacji Animacja działa tylko nie w tym kierunku

--- Robot/KR8R2100HW/DrawingVectors.py
+++ Robot/KR8R2100HW/DrawingVectors.py
@@ -3,13 +3,14 @@ from random import randrange
 import numpy as np
 from numpy import pi
 import matplotlib.pyplot as plt
-from matplotlib.animation import FuncAnimation
-from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.animation as animation
+
 from RobotDefinition import robotKR8R2100HW
 from RobotDefinition import Robot
 import copy
 
-def drawindA3DChart(robot,theraVar):
+
+def drawindA3DChart(robot, theraVar):
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
 
@@ -27,80 +28,95 @@ def drawindA3DChart(robot,theraVar):
     history_len = 500  # how many trajectory points to display
     dt = 0.1
     t = np.arange(0, t_stop, dt)  # create a time array from 0..t_stop sampled at dt steps
-    # line, = ax.plot([], [], [], 'o-', lw=2)
+    lines = [ax.plot([], [], [])[0] for _ in range(len(t))]
     # trace, = ax.plot([], [], [],'.-', lw=1, ms=2)
     # time_template = 'time = %.1fs'
     # time_text = ax.text(0.05, 0.9, '', transform=ax.transAxes)
 
-
+    np.set_printoptions(precision=6, suppress=True)
     dtheta = [] * len(robot)
     robotTrajectory = [Robot] * len(t)
-    dx, dy, dz = [[] * len(robot)] * len(t), [[] * len(robot)] * len(t), [[] * len(robot)] * len(t)
+    robotTrajectoryMatrix = np.empty((len(t), len(robot), 3))
+    robotTrajectoryDirMatrix = np.empty((len(t), len(robot), 3))
+    # trajectoryX, trajectoryY, trajectoryZ = [[] for _ in range(len(robot))], [[] for _ in range(len(robot))], [[] for _ in range(len(robot))]
+    # trajectoryDirX, trajectoryDirY, trajectoryDirZ = [[] for _ in range(len(robot))], [[] for _ in range(len(robot))], [[] for _ in range(len(robot))]
 
     # Różniczkowanie zmiany kąta robota
     for i in range(len(robot)):
         dtheta.append(robot[i].theta + ((theraVar[i] * t[:]) / t_stop))
 
-
-    #U torzenie trajektoru ruchu robota
+    # U torzenie trajektoru ruchu robota
     for dt in range(len(t)):
         robotTrajectory[dt] = copy.deepcopy(robot)
         for i in range(len(dtheta)):
-            robotTrajectory[dt].update_axis_theta(axis_index= i,new_theta= dtheta[i][dt])
+            robotTrajectory[dt].update_axis_theta(axis_index=i, new_theta=dtheta[i][dt])
             robotTrajectory[dt].get_total_transformation_matrix()
-            dx[i].append(robotTrajectory[dt][i].pos['x'])
-            dy[i].append(robotTrajectory[dt][i].pos['y'])
-            dz[i].append(robotTrajectory[dt][i].pos['z'])
 
+            robotTrajectoryMatrix[dt][i][0] = robotTrajectory[dt][i].pos['x']  # Pozycja X
+            robotTrajectoryMatrix[dt][i][1] = robotTrajectory[dt][i].pos['y']  # Pozycja Y
+            robotTrajectoryMatrix[dt][i][2] = robotTrajectory[dt][i].pos['z']  # Pozycja Z
 
+            robotTrajectoryDirMatrix[dt][i][0] = robotTrajectory[dt][i].dir['x']  # Pozycja X
+            robotTrajectoryDirMatrix[dt][i][1] = robotTrajectory[dt][i].dir['y']  # Pozycja Y
+            robotTrajectoryDirMatrix[dt][i][2] = robotTrajectory[dt][i].dir['z']  # Pozycja Z
+
+            # trajectoryX[i].append(robotTrajectory[dt][i].pos['x'])
+            # trajectoryY[i].append(robotTrajectory[dt][i].pos['y'])
+            # trajectoryZ[i].append(robotTrajectory[dt][i].pos['z'])
+            #
+            # trajectoryDirX[i].append(robotTrajectory[dt][i].dir['x'])
+            # trajectoryDirY[i].append(robotTrajectory[dt][i].dir['y'])
+            # trajectoryDirZ[i].append(robotTrajectory[dt][i].dir['z'])
+
+    print(robotTrajectoryMatrix)
 
     # Aktualizaja osi robota
     for i in range(len(robot)):
         robot.update_axis_theta(axis_index=i, new_theta=robot[i].theta + theraVar[i])
     robot.get_total_transformation_matrix()
 
-    transparetnDt = np.arange(0.1, 1, 0.9/len(t))
-    transparetnDt = list(map(lambda x: round(x,3), transparetnDt))
-    print((transparetnDt))
+    transparetnDt = np.arange(0.1, 1, 0.9 / len(t))
+    transparetnDt = list(map(lambda x: round(x, 3), transparetnDt))
+
+    print(transparetnDt)
 
     for i in range(len(robot)):
         r, g, b = randrange(0, 2), randrange(0, 2), randrange(0, 2),
 
-        for dt in range(len(t)):
-            robotTrajectoryChaty = ax.quiver(robotTrajectory[dt][i].pos["x"], robotTrajectory[dt][i].pos["y"], robotTrajectory[dt][i].pos["z"],
-                                             robotTrajectory[dt][i].dir["x"], robotTrajectory[dt][i].dir["y"], robotTrajectory[dt][i].dir["z"],
-                                             length=robot[i].lenght, normalize=True, color=(r, g, b, transparetnDt[dt]))
-
+        # for dt in range(len(t)):
+        #     robotTrajectoryChaty = ax.quiver(robotTrajectory[dt][i].pos["x"], robotTrajectory[dt][i].pos["y"],
+        #                                      robotTrajectory[dt][i].pos["z"],
+        #                                      robotTrajectory[dt][i].dir["x"], robotTrajectory[dt][i].dir["y"],
+        #                                      robotTrajectory[dt][i].dir["z"],
+        #                                      length=robot[i].lenght, normalize=True, color=(r, g, b, transparetnDt[dt]))
 
         endPos = ax.quiver(robot[i].pos["x"], robot[i].pos["y"], robot[i].pos["z"],
                            robot[i].dir["x"], robot[i].dir["y"], robot[i].dir["z"],
                            length=robot[i].lenght, normalize=True, color=(r, g, b, 1))
         endText = ax.text(robot[i].pos["x"], robot[i].pos["y"], robot[i].pos["z"], f'({(robot[i].pos["x"]):2.2f},'
-                                                                                     f' {(robot[i].pos["y"]):2.2f},'
-                                                                                     f' {(robot[i].pos["z"]):2.2f},'
-                                                                                     f' {math.degrees(robot[i].theta):2.2f}[°])')
+                                                                                   f' {(robot[i].pos["y"]):2.2f},'
+                                                                                   f' {(robot[i].pos["z"]):2.2f},'
+                                                                                   f' {math.degrees(robot[i].theta):2.2f}[°])')
 
-    def animate(i):
-        # thisx = [robotTrajectory[:][0].pos['x'], x1[i], x2[i]]
-        # thisy = [0, y1[i], y2[i]]
-        # thisz = [0, y1[i], y2[i]]
-        #
-        # history_x = x2[:i]
-        # history_y = y2[:i]
-        #
-        # line.set_data(thisx, thisy)
-        # line.set_3d_properties(walk[:num, 2])
-        trace.set_data(history_x, history_y)
-        time_text.set_text(time_template % (i * dt))
-        return line, trace, time_text
+    def animate(i, pos, dir):
+        for line, pos in zip(dir, pos):
+            # NOTE: there is no .set_data() for 3 dim data...
+            line.set_data(pos[:i, :2].T)
+            line.set_3d_properties(pos[:i, 2])
+        return lines
+
+    ani = animation.FuncAnimation(
+        fig, animate, len(t), fargs=(robotTrajectoryMatrix, lines), interval=100)
     plt.show()
+
 
 def enterRobotRotations():
     Fivar = []
 
     for i in range(6):
         try:
-            FiInput = math.radians(float(input(f"Podaj obrót w okół osi A{i + 1} (lub wpisz 'koniec' aby zakończyć): ")))
+            FiInput = math.radians(
+                float(input(f"Podaj obrót w okół osi A{i + 1} (lub wpisz 'koniec' aby zakończyć): ")))
             if FiInput == 'koniec':
                 break
 
@@ -113,6 +129,7 @@ def enterRobotRotations():
 
     return Fivar
 
+
 if __name__ == "__main__":
     # Wprowadzanie i rysowanie pierwszego zestawu danych
     theraVar = [0] * 6
@@ -120,7 +137,7 @@ if __name__ == "__main__":
     for axis in robot:
         print(axis)
     print(robot)
-    drawindA3DChart(robot,theraVar)
+    drawindA3DChart(robot, theraVar)
 
     # Aktualizowanie wykresu dla kolejnych zestawów danych
     while True:
@@ -129,6 +146,6 @@ if __name__ == "__main__":
             break
 
         theraVar = enterRobotRotations()
-        #robotStart = copy.deepcopy(robot)
+        # robotStart = copy.deepcopy(robot)
 
-        drawindA3DChart(robot,theraVar)
+        drawindA3DChart(robot, theraVar)
--- Testy/Testy.py
+++ Testy/Testy.py
@@ -1,3 +1,4 @@
+import numpy as np
 # List of strings
 l = ['sat', 'bat', 'cat', 'mat']
 
@@ -21,3 +22,31 @@ testStr = ''
 for x in test:
     testStr += ''.join(x) + ' '
 print(testStr)
+
+randomVar = np.random.random(3)
+print(type(randomVar))
+max_step=0.05
+steps = np.random.uniform(-max_step, max_step, size=(30, 3))
+print(np.random.random(3))
+print(steps)
+
+import numpy as np
+
+# Przygotowanie danych
+liczba_probek = 25  # Ilość próbek czasu
+liczba_osi = 6  # Ilość osi
+
+# Inicjalizacja tablicy dla każdej osi
+robotTrajectory = np.empty((liczba_probek, liczba_osi, 3))
+
+# Pętla po czasie (dt)
+for dt in range(liczba_probek):
+    # Pętla po osiach (i)
+    for i in range(liczba_osi):
+        # Przykładowe dane - możesz je dostosować do swoich potrzeb
+        robotTrajectory[dt][i][0] = np.random.rand()  # Pozycja X
+        robotTrajectory[dt][i][1] = np.random.rand()  # Pozycja Y
+        robotTrajectory[dt][i][2] = np.random.rand()  # Pozycja Z
+
+# Wyświetlenie przykładowych danych
+print(robotTrajectory)
